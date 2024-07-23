3e82375f14c7473760f35ba545855f67fb889f43
Dmitriy-Mamontov <dimik94-26@mail.ru>
2024-07-23 15:19:06 +0300

    Added new files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<main>
+			<div class="wrapper">
+				<input class="word" v-model="inputValue" placeholder="0" />
+			</div>
+			<the-button 
+			@addNumber="addNumber"
+			@allClean="allClean"
+			@addOperator="addOperator"
+			@showResult="showResult"
+			@squareRoot="squareRoot"
+			@calcPercent="calcPercent"
+			@deleteLastSymbol="deleteLastSymbol"
+			></the-button>
+		</main>
 </template>
+<!--          TODO: Сделай переиспользуемый компонент кнопки, компоненты хранятся в папке components #--> 
+
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheButton from '../Components/TheButton.vue'
+export default{
+  // TODO: Убери неиспользуемый код (комментарии)#
+	components:{
+		TheButton,
+	},
+	data(){
+		return{
+			inputValue:'',
+		}
+	},
+	methods:{
+		addNumber(data){
+			this.inputValue = Object.values(data)
+		},
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+		allClean(){
+			this.inputValue = ''
+		},
+
+		addOperator(data){
+			this.inputValue = Object.values(data)
+		},
+
+		showResult(data){
+			this.inputValue = Object.values(data)
+		},
+
+		squareRoot(data){
+			this.inputValue = Object.values(data)
+		},
+
+		calcPercent(data){
+			this.inputValue = Object.values(data)
+		},
+
+		deleteLastSymbol(data){
+			this.inputValue = Object.values(data)
+		}
+		// TODO: Пробелы не нужны после объявления методов (+ if else конструкции (else пишется на той же строке, где заканчивается if)#)
+    // TODO: Между методами 1 вместо 2
+	},
+
+// TODO: Убери ненужные пробелы#
 }
 </script>
+<!--TODO: Стили пишутся везде с атрибутом scoped #-->
+/* TODO: Сделай для одной страницы названия классов в стиле БЭМ +
+добавь в тег style атрибут lang="scss", используя препроцессор, перепиши стили для калька
+*/
+<style lang="scss" scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@mixin wh ($w, $h){
+	width:$w;
+	height:$h;
 }
+html{
+@include wh(100%, 100%)
+}
+body{
+	@include wh(100%, 100%);
+	box-sizing: content-box
+}
+#app{
+	@include wh(100%, 100%)
+}
+main{
+	max-width:25%;
+	min-width: 120px;
+	margin: 150px auto;
+	.wrapper{
+		@include wh(100%, 50px);
+		padding: 0;
+	}
+	.word{
+		@include wh(100%, 100%);
+		margin: 0;
+		box-sizing: border-box;
+		padding: 2px;
+		border: 1px solid black;
+		background-color: black;
+		color:white;
+		font-size: large;
+	}
+}
+@media (max-width:700px){
+		main{
+			max-width: 120px;
+		}
+	}
+/*TODO: стили прописываются по иерархии от большего к меньшему, html, body, #app, main, .classes, etc*/
 </style>
